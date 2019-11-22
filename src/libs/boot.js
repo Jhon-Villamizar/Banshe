39b6566befdf56f64cc0d95abe2f7a150d1377ff
Jhon Villamizar <jvillamizaro@chefcompany.co>
2019-11-22 18:35:54 -0500

    edito boot

--- src/libs/boot.js
+++ src/libs/boot.js
@@ -1,13 +1,5 @@
 module.exports = app => {
-var https = require('https');
-
-
-      app.db.sequelize.sync().done(()=>{ //crea tablas en cada peticion con metodos de sequelize por orden de ejecucuion
-        app.listen(app.get('port'), () => {
-          console.log('Server on port', app.get('port'));
-        });
-      });
-      
-
-  
-  };
+  app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+  });
+};
